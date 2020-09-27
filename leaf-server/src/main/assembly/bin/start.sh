11e8bafae654b0c1562c4283bce6833bcaefd27c
zhangmc <zhangmc@qtrade.com.cn>
2020-09-27 16:05:33 +0800

    config: 启动脚本命令精简

--- leaf-server/src/main/assembly/bin/start.sh
+++ leaf-server/src/main/assembly/bin/start.sh
@@ -46,7 +46,8 @@ fi
 STDOUT_FILE=$LOGS_DIR/stdout.log
 
 LIB_DIR=$DEPLOY_DIR/lib
-LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
+#LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
+LIB_JARS=$LIB_DIR/*
 
 JAVA_OPTS=" -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true "
 JAVA_DEBUG_OPTS=""
